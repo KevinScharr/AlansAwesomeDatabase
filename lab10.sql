e8bf7651736d5c4fcc8f93d0fc6f25aff3d4abb4
KevinScharr <kevin.scharr1@marist.edu>
2016-04-19 09:34:56 -0400

    Lab 10
    Submitting lab 10 despite errors with input syntax.

---
+++ lab10.sql
@@ -0,0 +1,43 @@
+﻿create or replace function PreReqsFor
+(
+cursNum Int
+)
+returns int
+as $$
+declare 
+   preReq int;
+   resultSet refcursor;
+Begin
+   Open resultSet for
+     select preReqNum
+     from Prerequisites
+     where courseNum = cursNum;
+   return resultSet;
+End;
+$$
+Language PLPGSQL;
+
+select PreReqsFor(499);
+
+
+
+create or replace function isPreReqsFor
+(
+preReq Int
+)
+returns int
+as $$
+declare 
+   cursNum int;
+   resultSet refcursor;
+Begin
+   Open resultSet for
+     select courseNum
+     from Prerequisites
+     where preReqNum = preReq;
+   return resultSet;
+End;
+$$
+Language PLPGSQL;
+
+select isPreReqsFor(308);
